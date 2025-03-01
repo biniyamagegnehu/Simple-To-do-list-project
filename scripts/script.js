8a128e4a22187e51574a44219dcdecafff5489ab
biniyamagegnehu <biniyamagegnehu22gmail.com>
2025-03-01 22:33:03 +0300

    adding remove button

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,39 @@
 const toDoList = [];
 
+let task;
+
+displayTasks();
+
+function displayTasks() {
+    let listHTML = '';
+
+    toDoList.forEach(() => {
+        const index = toDoList.indexOf(task);
+
+        listHTML += `
+            <p>
+                ${task}
+                <button onclick="
+                    toDoList.splice(${index},1);
+                    displayTasks();
+                ">
+                    Remove
+                </button>
+            </p>
+        `;
+    });
+    
+    document.querySelector('.display-tasks').innerHTML = listHTML;
+}
+
 function addTask() {
     const inputTask = document.querySelector('input');
-    const task = inputTask.value;
+    task = inputTask.value;
 
     toDoList.push(task);
-    document.querySelector('p').innerHTML = toDoList.join('<br>');
+    inputTask.value = '';
+
+    displayTasks();
 }
 
 
