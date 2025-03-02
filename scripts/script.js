0a57493e4e9614c787d0d97efd224cacee0ff0f8
biniyamagegnehu <biniyamagegnehu22gmail.com>
2025-03-02 10:55:21 +0300

    adding date and diplay task date

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,47 @@
-const toDoList = [];
-
-let task;
+const toDoList = [{}];
 
 displayTasks();
 
 function displayTasks() {
     let listHTML = '';
 
-    toDoList.forEach(() => {
-        const index = toDoList.indexOf(task);
+    for (let index = 0; index < toDoList.length; index++) {
+        const taskObject = toDoList[index];
+        const task = taskObject.task;
+        const taskDate = taskObject.taskDate;
 
         listHTML += `
-            <p>
+            <p>              
                 ${task}
+                ${taskDate}
                 <button onclick="
                     toDoList.splice(${index},1);
                     displayTasks();
                 ">
                     Remove
                 </button>
+            
             </p>
         `;
-    });
+        
+    }
+
     
     document.querySelector('.display-tasks').innerHTML = listHTML;
 }
 
 function addTask() {
-    const inputTask = document.querySelector('input');
-    task = inputTask.value;
+    const inputTask = document.querySelector('.task');
+    const inputDate = document.querySelector('.task-date');
+    let task = inputTask.value;
+    let taskDate = inputDate.value;
 
-    toDoList.push(task);
+    toDoList.push({
+        task:task,
+        taskDate:taskDate
+    });
+    console.log(toDoList);
     inputTask.value = '';
 
     displayTasks();
 }
-
-
